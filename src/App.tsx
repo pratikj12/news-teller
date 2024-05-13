51f9021b72b90a7f00e5c57090d324c0c143c2ef
pratik jadhav <jadhavpratikraje1998@gmail.com>
2024-05-13 19:48:42 +0530

    News-App-React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import NewsContainer from "./components/news-container";
+
+function App() {
+  return (
+    <div className="App">
+      <NewsContainer />
+    </div>
+  );
+}
+
+export default App;
